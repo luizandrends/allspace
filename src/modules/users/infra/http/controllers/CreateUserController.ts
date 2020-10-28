eb6ba88c2789976eb3044f46e73a9ac91dc9e648
Luiz Andre <luizxd234@gmail.com>
2020-10-28 20:38:38 -0300

    feat: controller and route

---
+++ src/modules/users/infra/http/controllers/CreateUserController.ts
@@ -0,0 +1,25 @@
+import { container } from 'tsyringe';
+import { Request, Response } from 'express';
+
+import CreateUserService from '@modules/users/services/CreateUserService';
+
+class CreateUserController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { name, email, cpf, password } = request.body;
+
+    const createUserService = container.resolve(CreateUserService);
+
+    const userData = {
+      name,
+      email,
+      cpf,
+      password,
+    };
+
+    const user = await createUserService.execute(userData);
+
+    return response.json(user);
+  }
+}
+
+export default CreateUserController;
---
+++ src/modules/users/infra/http/routes/user.routes.ts
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
+
+import UserController from '../controllers/CreateUserController';
+
+const userController = new UserController();
+
+const userRouter = Router();
+
+userRouter.post(
+  '/create',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      cpf: Joi.string().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  userController.create
+);
+
+export default userRouter;
