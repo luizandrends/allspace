85aeed4f07d92f6eaaeee330f29db3fd31104f14
Luiz Andre <luizxd234@gmail.com>
2020-11-03 20:21:46 -0300

    feat: Interfaces, entities, repositories DI

--- src/shared/infra/container/index.ts
+++ src/shared/infra/container/index.ts
@@ -5,7 +5,15 @@ import '@modules/users/providers';
 import IUsersInterface from '@modules/users/interfaces/IUsersInterface';
 import UsersRepository from '@modules/users/infra/database/repositories/UsersRepository';
 
+import IUserTokensInterface from '@modules/users/interfaces/IUserTokensInterface';
+import UserTokensRepository from '@modules/users/infra/database/repositories/UserTokensRepository';
+
 container.registerSingleton<IUsersInterface>(
   'UsersRepository',
   UsersRepository
+);
+
+container.registerSingleton<IUserTokensInterface>(
+  'UserTokensRepository',
+  UserTokensRepository
 );
