6c889eb6bcdc768942e34894a17adf01e737bd6a
Luiz Andre Nunes da Silva <luizxd234@gmail.com>
2020-12-07 18:12:36 -0300

    chore: Create advert service

--- src/modules/adverts/services/CreateAdvertService.ts
+++ src/modules/adverts/services/CreateAdvertService.ts
@@ -1,6 +1,7 @@
 import { injectable, inject } from 'tsyringe';
 
 import IUsersInterface from '@modules/users/interfaces/IUsersInterface';
+import AppError from '@shared/errors/AppError';
 import Advert from '../infra/database/entities/Advert';
 
 import IAdvertsInterface from '../interfaces/IAdvertsInterface';
@@ -17,7 +18,19 @@ class CreateAdvertService {
     private advertsRepository: IAdvertsInterface
   ) {}
 
-  public async execute(advertData: IAdvertDTO): Promise<Advert> {}
+  public async execute(advertData: IAdvertDTO): Promise<Advert> {
+    const { user_id } = advertData;
+
+    const findUserById = await this.usersRepository.findById(user_id);
+
+    if (!findUserById) {
+      throw new AppError('User not found', 400);
+    }
+
+    const advert = await this.advertsRepository.create(advertData);
+
+    return advert;
+  }
 }
 
 export default CreateAdvertService;
