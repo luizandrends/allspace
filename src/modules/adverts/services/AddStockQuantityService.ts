dfb7ccc36bea372216bd0b494ed12ca0460ad23f
Luiz Andre Nunes da Silva <luizxd234@gmail.com>
2020-12-08 18:56:22 -0300

    feat: AddStockQuantity

--- src/modules/adverts/services/AddStockQuantityService.ts
+++ src/modules/adverts/services/AddStockQuantityService.ts
@@ -1,5 +1,7 @@
 import { injectable, inject } from 'tsyringe';
 
+import AppError from '@shared/errors/AppError';
+
 import Advert from '../infra/database/entities/Advert';
 
 import IAdvertsInterface from '../interfaces/IAdvertsInterface';
@@ -16,7 +18,20 @@ class CreateAdvertService {
     private advertsRepository: IAdvertsInterface
   ) {}
 
-  public async execute({ advert_id, quantity }: IRequest): Promise<Advert> {}
+  public async execute({ advert_id, quantity }: IRequest): Promise<Advert> {
+    const findAdvert = await this.advertsRepository.findById(advert_id);
+
+    if (!findAdvert) {
+      throw new AppError('Unexistent advert', 400);
+    }
+
+    const increaseStock = await this.advertsRepository.increaseStockQuantitiy(
+      advert_id,
+      quantity
+    );
+
+    return increaseStock;
+  }
 }
 
 export default CreateAdvertService;
